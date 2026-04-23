32cbbd097aa60d4770e1f8efc364d8863e65e0c1
César Guerra <warderer@gmail.com>
2026-04-23 12:56:37 -0600

    añadi funcion de generar excusa y boton

--- 05-Generador-Excusas/generadorExcusas.js
+++ 05-Generador-Excusas/generadorExcusas.js
@@ -39,10 +39,14 @@ const columnaC = [
   "entregamos sin errores (esperemos)"
 ];
 
-const excusaA = columnaA[Math.floor(Math.random() * columnaA.length)];
-const excusaB = columnaB[Math.floor(Math.random() * columnaB.length)];
-const excusaC = columnaC[Math.floor(Math.random() * columnaC.length)];
+function generarExcusa() {
+    const excusaA = columnaA[Math.floor(Math.random() * columnaA.length)];
+    const excusaB = columnaB[Math.floor(Math.random() * columnaB.length)];
+    const excusaC = columnaC[Math.floor(Math.random() * columnaC.length)];
+    
+    const excusaFinal = `Mi excusa es: ${excusaA} ${excusaB} ${excusaC}.`;
+    
+    document.getElementById("excusa").textContent = excusaFinal;
+}
 
-const excusaFinal = `Mi excusa es: ${excusaA} ${excusaB} ${excusaC}.`;
-
-document.getElementById("excusa").textContent = excusaFinal;
+generarExcusa();
